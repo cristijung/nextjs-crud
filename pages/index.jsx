24da33332148c07ec1a30141b0d10d7646bb6a17
Maria Cristina Jung Muller <cristina.jung@dbccompany.com.br>
2022-09-22 23:38:30 -0300

    remove

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ function Home() {
             <h1>Next.js: CRUD + Hooks</h1>
             <p>
                 Um App de exemplo mostrando como listar, adicionar, editar e excluir 
-                registros de usuário com Next.js 10 e a biblioteca React Hook Form.
+                registros de usuário com Next.js e a biblioteca React Hook Form.
             </p>
             <p><Link href="/users">&gt;&gt; Gerenciando usuários</Link></p>
         </div>
